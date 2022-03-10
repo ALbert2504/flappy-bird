d2f9661b07bee1a22c73dada2937e9e1fe641773
Albert Arakelyan <albertarakelyan1998@gmail.com>
2022-03-10 23:46:58 +0400

    initial commit

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1646941366,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/gamebg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipeup-sheet0.png",
+		"images/floorbg.png",
+		"images/backbtn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
